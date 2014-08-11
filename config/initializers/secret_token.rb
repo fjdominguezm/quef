859412deaeea0db12f713cb49f7b7281d529f5b7
fjdominguezm <fjdominguezm@gmail.com>
2014-08-11 14:31:34 +0200

    touch README.md git init git add README.md git commit -m "first commit" git remote add origin https://github.com/fjdominguezm/quef.git git push -u origin master

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebAppQuEF::Application.config.secret_key_base = '6dee6c010aaf2de3518ad3f3e95794d207beccf3bee1d33b5e63cdcb622f5797fb0d333f6ca809d8eaf500a9cc1e5bebfe79839511eb90f77c958ec816a22883'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WebAppQuEF::Application.config.session_store :cookie_store, key: '_WebAppQuEF_session'
